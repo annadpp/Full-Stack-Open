010ed15079a7655c7ef0da3c725c3bcf1ea73587
annadpp <anna.depablo.puig@gmail.com>
2023-09-24 16:15:50 +0200

    Exercise 2.1

---
+++ Part2/CourseInfo/src/components/Course.jsx
@@ -0,0 +1,28 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </>
+);
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
