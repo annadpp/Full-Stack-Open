a5a8e1e3295e6e85f266486983a75a237affd11f
annadpp <anna.depablo.puig@gmail.com>
2023-11-23 16:06:40 +0100

    Exercises 3.7 + 3.8

--- Part3/PhonebookBackend/index.js
+++ Part3/PhonebookBackend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+
+// Define a new token for Morgan
+morgan.token("post-body", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postData"
+  )
+);
 
 let persons = [
   {
