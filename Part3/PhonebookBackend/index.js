fb37519dbbaa6ab1c5c54aa824b920a8ab790d02
annadpp <anna.depablo.puig@gmail.com>
2023-11-23 14:24:42 +0100

    Exercise 3.5

--- Part3/PhonebookBackend/index.js
+++ Part3/PhonebookBackend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -56,6 +57,30 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000) + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
