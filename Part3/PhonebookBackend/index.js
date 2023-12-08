de2fbf96bdabb0c15729aedc7efbe642e14dcb8b
annadpp <anna.depablo.puig@gmail.com>
2023-12-08 17:11:49 +0100

    Exercise 3.14

--- Part3/PhonebookBackend/index.js
+++ Part3/PhonebookBackend/index.js
@@ -22,29 +22,6 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -64,50 +41,45 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(id)
+    .then(() => response.status(204).end())
+    .catch((error) => response.status(400).json({ error: "malformatted id" }));
 });
-
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000) + 1;
-};
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
+  const name = body.name;
+  const number = body.number;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
+  if (!name) return response.status(400).json({ error: "name missing" });
+  if (!number) return response.status(400).json({ error: "number missing" });
 
-  const duplicateName = persons.find((person) => person.name === body.name);
-  if (duplicateName) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  Person.exists({ name }).then((personExists) => {
+    if (personExists) {
+      return response.status(400).json({ error: "name must be unique" });
+    }
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(person);
-
-  response.json(person);
+    const newPerson = new Person({ name, number });
+    newPerson
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => {
+        response.status(500).json({ error: "Failed to save to the database" });
+      });
+  });
 });
 
 const PORT = process.env.PORT || 3001;
