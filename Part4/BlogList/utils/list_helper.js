7a8a0e85203a0ec932ebcd89daed20952a953782
annadpp <anna.depablo.puig@gmail.com>
2023-12-12 16:38:25 +0100

    Exercise 4.5

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -2,6 +2,29 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return total;
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const favorite = blogs.reduce((favoriteBlog, currentBlog) => {
+    return currentBlog.likes > favoriteBlog.likes ? currentBlog : favoriteBlog;
+  }, blogs[0]);
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
