382e5bcacee34bd0b15979a9349f9ca1a671355a
annadpp <anna.depablo.puig@gmail.com>
2024-02-09 12:56:44 +0100

    Exercise 5.22

--- Part5/BlogListFrontend/cypress/e2e/bloglist_app.cy.js
+++ Part5/BlogListFrontend/cypress/e2e/bloglist_app.cy.js
@@ -6,7 +6,15 @@ describe("Bloglist app", function () {
       username: "testuserone",
       password: "testnumberone",
     };
+
+    const usertwo = {
+      name: "testtwo",
+      username: "testusertwo",
+      password: "testnumbertwo",
+    };
+
     cy.request("POST", "http://localhost:3003/api/users/", user);
+    cy.request("POST", "http://localhost:3003/api/users/", usertwo);
     cy.visit("http://localhost:3000");
   });
 
@@ -58,15 +66,43 @@ describe("Bloglist app", function () {
         cy.contains("test title test author");
       });
 
-      it.only("the user can like it", function () {
+      it("the user can like it", function () {
         cy.contains("view").click();
         cy.contains("0").contains("likes").click();
         cy.contains("1");
       });
 
-      it("user can delete their blogs", function () {
+      it("the user can delete their blogs", function () {
         cy.contains("view").click();
         cy.get("#remove-button").click();
+        cy.contains("remove").should("not.exist");
+      });
+    });
+
+    describe("when there are > one users", function () {
+      beforeEach(function () {
+        cy.contains("logout").click();
+        cy.get("#username").type("testusertwo");
+        cy.get("#password").type("testnumbertwo");
+        cy.get("#login-button").click();
+        cy.contains("usertwo logged in");
+
+        cy.contains("create new blog").click();
+        cy.get("#title").type("test title two");
+        cy.get("#author").type("test author two");
+        cy.get("#url").type("http://testtwo.com");
+        cy.get("#create-button").click({ force: true });
+        cy.contains("test title two test author two");
+      });
+
+      it.only("only the user who created the blog can delete it", function () {
+        cy.contains("logout").click();
+        cy.get("#username").type("testuserone");
+        cy.get("#password").type("numberone");
+        cy.get("#login-button").click();
+        cy.contains("one logged in");
+
+        cy.contains("view").click();
         cy.contains("remove").should("not.exist");
       });
     });
