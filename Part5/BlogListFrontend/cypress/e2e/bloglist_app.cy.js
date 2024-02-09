e8b8d0e8588c99b7a1dcc96af24e8a4b5d6499a4
annadpp <anna.depablo.puig@gmail.com>
2024-02-09 11:55:16 +0100

    Exercise 5.18

--- Part5/BlogListFrontend/cypress/e2e/bloglist_app.cy.js
+++ Part5/BlogListFrontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe("Bloglist app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "testone",
+      username: "testuserone",
+      password: "testnumberone",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
@@ -8,5 +14,20 @@ describe("Bloglist app", function () {
     cy.contains("Log in to application");
     cy.contains("username");
     cy.contains("password");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("testuserone");
+      cy.get("#password").type("testnumberone");
+      cy.get("#login-button").click();
+      cy.contains("testone logged in");
+    });
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("testuserone");
+      cy.get("#password").type("wrongpassword");
+      cy.get("#login-button").click();
+      cy.get(".error").contains("Wrong username or password");
+    });
   });
 });
