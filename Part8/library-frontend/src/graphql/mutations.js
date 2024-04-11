726bcf41c47e9c3591af561cf7511d472f07013a
annadpp <anna.depablo.puig@gmail.com>
2024-04-11 12:05:55 +0200

    Exercises 8.1-8.12 resurrected

---
+++ Part8/library-frontend/src/graphql/mutations.js
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client";
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $author: String!
+    $genres: [String!]!
+    $published: Int!
+    $title: String!
+  ) {
+    addBook(
+      author: $author
+      genres: $genres
+      published: $published
+      title: $title
+    ) {
+      id
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation EditAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      born
+      id
+    }
+  }
+`;
---
+++ Part8/library-frontend/src/graphql/queries.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+  query allAuthors {
+    allAuthors {
+      bookCount
+      born
+      id
+      name
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query allBooks {
+    allBooks {
+      author
+      id
+      published
+      title
+    }
+  }
+`;
